2106d50db1f68a3638c8bb317cfede070d43acd1
nox-cyber0 <mhabubakar999@outlook.com>
2024-03-03 12:54:06 +0000

    navbar update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,26 @@
-import "@/styles/globals.css";
-import { Metadata } from "next";
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Providers } from "./providers";
-import { Navbar } from "@/components/navbar";
-import { Link } from "@nextui-org/link";
-import clsx from "clsx";
+import '@/styles/globals.css';
+import { Metadata } from 'next';
+import { siteConfig } from '@/config/site';
+import { fontSans } from '@/config/fonts';
+import { Providers } from './providers';
+import Navbar from '@/components/navbar';
+import { Link } from '@nextui-org/link';
+import clsx from 'clsx';
 
-export const metadata: Metadata = {
+export const viewport: Metadata = {
 	title: {
 		default: siteConfig.name,
 		template: `%s - ${siteConfig.name}`,
 	},
 	description: siteConfig.description,
 	themeColor: [
-		{ media: "(prefers-color-scheme: light)", color: "white" },
-		{ media: "(prefers-color-scheme: dark)", color: "black" },
+		{ media: '(prefers-color-scheme: light)', color: 'white' },
+		{ media: '(prefers-color-scheme: dark)', color: 'black' },
 	],
 	icons: {
-		icon: "/favicon.ico",
-		shortcut: "/favicon-16x16.png",
-		apple: "/apple-touch-icon.png",
+		icon: '/favicon.ico',
+		shortcut: '/favicon-16x16.png',
+		apple: '/apple-touch-icon.png',
 	},
 };
 
@@ -30,29 +30,32 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en" suppressHydrationWarning>
+		<html
+			lang='en'
+			suppressHydrationWarning
+		>
 			<head />
 			<body
 				className={clsx(
-					"min-h-screen bg-background font-sans antialiased",
+					'min-h-screen bg-background font-sans antialiased',
 					fontSans.variable
 				)}
 			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-					<div className="relative flex flex-col h-screen">
+				<Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+					<div className='relative flex flex-col h-screen'>
 						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+						<main className='container mx-auto max-w-7xl pt-16 px-6 flex-grow'>
 							{children}
 						</main>
-						<footer className="w-full flex items-center justify-center py-3">
+						<footer className='w-full flex items-center justify-center py-3'>
 							<Link
 								isExternal
-								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-								title="nextui.org homepage"
+								className='flex items-center gap-1 text-current'
+								href='https://nextui-docs-v2.vercel.app?utm_source=next-app-template'
+								title='nextui.org homepage'
 							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
+								<span className='text-default-600'>Powered by</span>
+								<p className='text-primary'>NextUI</p>
 							</Link>
 						</footer>
 					</div>
